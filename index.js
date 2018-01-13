a19a7f9a555aa3f4c97c5db1d40e6893dada311b
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2018-01-13 11:59:01 +0200

    regex and str build on direct contact

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+var emailRegex = require('regex-email');
 
 //EXPRESSJS SETUP
 var app = express();
@@ -8,7 +9,6 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-
 
 
 //NODEMAILER TRANSPORTER OBJECT SETUP
@@ -21,29 +21,37 @@ var transporter = nodemailer.createTransport({
 });
 
 //NEW DIRECT CONTACT
-app.post('/direct-contact', function(req, res){
-    var mailOptions = {
-        from: 'miller',
-        to: 'evanmillersolutions@gmail.com',
-        subject: 'New Direct Contact',
-        text: JSON.stringify(req.body)
-    };
+app.post('/direct-contact', function (req, res) {
+    if (emailRegex.test(req.body.email)) { //test email regex
+        var mailOptions = {
+            from: 'miller',
+            to: 'evanmillersolutions@gmail.com',
+            subject: 'New Direct Contact',
+            text: buildContactEmail(req.body)
+        };
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-
-    //res.redirect("http://localhost")
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Thank you! Your message has been sent and we will be in touch with you shortly. redirecting back in 3 seconds...');
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('Thank you! Your message has been sent and we will be in touch with you shortly.');
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('There seems to be a problem with the email you typed. Please try again.');
+    }
 });
 
-
+function buildContactEmail(data) {
+    email = "";
+    email += "Sender name: " + data.name;
+    email += "\nEmail: " + data.email;
+    email += "\nDetails: " + data.details;
+    return email;
+}
 
 
 const PORT = process.env.PORT || 8080;
